078d9836265d4098f3148adb38ab0c09bb965bfa
jakebeaver <1st.cubix@gmail.com>
2021-09-06 12:09:31 +0200

    better animations

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,12 +4,14 @@
 	import Sites from '../components/simple/sites.svelte';
 	import IconSwarm from '../components/simpleKinematics/iconSwarm.svelte';
 	import Cube from '../components/simpleKinematics/cube.svelte';
-	import { fly } from 'svelte/transition';
-	import { circOut } from 'svelte/easing';
+	import { fly, scale } from 'svelte/transition';
+	import { circOut, elasticOut, sineOut } from 'svelte/easing';
 	import { zIndex } from '../helpers/zIndexCalculator';
 	import { onMount } from 'svelte';
 
 	let show = true;
+	let clicked = false;
+	let acceptPointer = false;
 	onMount(() => {
 		setTimeout(() => {
 			show = false;
@@ -24,10 +26,18 @@
 <div class="fixed" style="z-index:{zIndex(1)}">
 	{#if show}
 		<Center>
-			<div out:fly={{ duration: 1000, y: -100, easing: circOut }}>
+			<div
+				out:fly={{
+					duration: clicked ? 1000 : 2000,
+					y: -200,
+					easing: clicked ? circOut : sineOut
+				}}
+				class="cursor-pointer"
+			>
 				<Pane
 					on:click={(x) => {
 						show = false;
+						clicked = true;
 					}}
 					width={300}
 					height={100}
@@ -38,7 +48,20 @@
 		</Center>
 	{:else}
 		<Center>
-			<div in:fly={{ delay: 500, duration: 1000, y: 100, easing: circOut }}>
+			<div
+				in:scale={{
+					delay: clicked ? 50 : 50,
+					duration: clicked ? 1500 : 4000,
+					start: 2,
+					easing: elasticOut
+				}}
+				on:introstart={() =>
+					setTimeout(() => {
+						acceptPointer = true;
+					}, 200)}
+				class:pointer-events-none={!acceptPointer}
+			>
+				<!-- <div in:fly={{ delay: 0, duration: 1000, y: 200, easing: circOut }}> -->
 				<Pane width={350} height={150}>
 					<Sites />
 				</Pane>
