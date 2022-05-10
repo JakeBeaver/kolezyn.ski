da721fe1c4245afd9ba382f0c6abfeda6789db57
jakebeaver <1st.cubix@gmail.com>
2022-05-10 23:52:58 +0200

    firebase google auth

---
+++ src/components/auth/loginButton.svelte
@@ -0,0 +1,90 @@
+<script lang="ts">
+	import { logIn, logOut, user } from './firebase.js';
+	const googleImgLink = 'https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg';
+</script>
+
+{#if $user.isLoggedIn}
+	<button class="fire-button" on:click={logOut}>
+		<span class="img-wrapper">
+			<img class="profile-img" alt={$user.name} src={$user.photoURL} />
+		</span>
+		<span class="text text-long">{$user.name} (Sign&nbsp;out)</span>
+		<span class="text text-short">Sign&nbsp;out</span>
+	</button>
+{:else}
+	<button class="fire-button" on:click={logIn}>
+		<span class="img-wrapper">
+			<img class="google-img" alt="google" src={googleImgLink} />
+		</span>
+		<span class="text text-long">Sign in with Google</span>
+		<span class="text text-short">Sign in </span>
+	</button>
+{/if}
+
+<style>
+	.img-wrapper {
+		display: table-cell;
+		vertical-align: middle;
+	}
+
+	.google-img {
+		border: none;
+		display: inline-block;
+		height: 18px;
+		vertical-align: middle;
+		width: 18px;
+	}
+	.profile-img {
+		border-radius: 50%;
+		border: none;
+		display: inline-block;
+		height: 40px;
+		vertical-align: middle;
+		width: 40px;
+	}
+
+	.text {
+		color: #fff;
+		display: table-cell;
+		font-size: 14px;
+		padding-left: 16px;
+		text-transform: none;
+		vertical-align: middle;
+	}
+
+	.text.text-long {
+		display: table-cell;
+	}
+
+	.text.text-short {
+		display: none;
+	}
+
+	@media (max-width: 210px) {
+		.text.text-long {
+			display: none;
+		}
+
+		.text.text-short {
+			display: table-cell;
+		}
+	}
+
+	.fire-button > .text {
+		color: #757575;
+	}
+
+	.fire-button {
+		border-radius:0.7em;
+		direction: ltr;
+		font-weight: 500;
+		height: auto;
+		line-height: normal;
+		max-width: 220px;
+		min-height: 40px;
+		padding: 8px 16px;
+		text-align: left;
+		width: 100%;
+		background-color: white;
+	}
+</style>
---
+++ src/routes/fun/chat/index.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import Center from '../../../components/simple/center.svelte';
+	import { fly, scale } from 'svelte/transition';
+	import { circOut, elasticOut } from 'svelte/easing';
+	import Pane from '../../../components/simple/pane.svelte';
+	import { user } from '../../../components/auth/firebase.js';
+	import LoginButton from '../../../components/auth/loginButton.svelte';
+</script>
+
+<div class="goToCorner">
+	<LoginButton />
+</div>
+
+<Center>
+	{#if !$user.isLoggedIn}
+		<div
+			out:fly={{
+				duration: 1000,
+				y: -200,
+				easing: circOut
+			}}
+		>
+			<Pane width={300} height={100}>
+				<p class="text-3xl font-bold opacity-100">Sign in to chat</p>
+			</Pane>
+		</div>
+	{:else}
+		<div
+			in:scale={{
+				delay: 50,
+				duration: 1500,
+				start: 2,
+				easing: elasticOut
+			}}
+		>
+			<Pane width={300} height={100}>
+				<p class="text-2xl font-bold opacity-100">There'll be something to write here maybe</p>
+			</Pane>
+		</div>{/if}
+</Center>
+
+<style>
+	.goToCorner {
+		z-index: 10000;
+		right: 10px;
+		top: 10px;
+		position: fixed;	
+	}
+</style>
